4053248c02b301933855f078f1cab89bdd72aabc
Wilson.Lin <wilson.lin@nadisystem.com>
2022-04-20 20:20:04 +0800

    day5

---
+++ context/server.go
@@ -0,0 +1,30 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // todo: log error however you like
+		}
+		fmt.Fprint(w, data)
+	}
+}
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}

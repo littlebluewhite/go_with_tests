85782fc14df103833254a835a00df9cff3e77522
Wilson.Lin <wilson.lin@nadisystem.com>
2022-04-27 19:00:12 +0800

    day7

---
+++ revisiting_generics/reduce.go
@@ -0,0 +1,76 @@
+package revisiting_generics
+
+func Reduce[A, B any](collection []A, accumulator func(B, A) B, initialValue B) B {
+	var result = initialValue
+	for _, x := range collection {
+		result = accumulator(result, x)
+	}
+	return result
+}
+
+// Sum calculates the total from a slice of numbers.
+func Sum(numbers []int) int {
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
+}
+
+// SumAllTails calculates the sums of all but the first number given a collection of slices.
+func SumAllTails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
+		} else {
+			tail := x[1:]
+			return append(acc, Sum(tail))
+		}
+	}
+
+	return Reduce(numbers, sumTail, []int{})
+
+}
+
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
+	}
+	if transaction.To == a.Name {
+		a.Balance += transaction.Sum
+	}
+	return a
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(
+		transactions,
+		applyTransaction,
+		account,
+	)
+}
+
+func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
+	for _, v := range items {
+		if predicate(v) {
+			return v, true
+		}
+	}
+	return
+}
+
+type Person struct {
+	Name string
+}
